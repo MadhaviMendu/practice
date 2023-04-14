6e6d10664f9573a5f184a0fa1adfa787fc62d4fa
Madhavi Mendu <madhaviabc@gmail.com>
2023-04-14 12:32:28 +0100

    good

---
+++ Python001.py
@@ -0,0 +1,5 @@
+# Write a program to reverse an integer in Python
+# Write a program in Python to check whether an integer is Armstrong number or not.
+# Write a program in Python to check given number is prime or not.
+# Write a program in Python to find greatest among three integers.
+
