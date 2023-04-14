09878ed87cb51717786c90e810b16027a0874111
Madhavi Mendu <madhaviabc@gmail.com>
2023-04-14 15:32:49 +0100

    changes

--- Python001.py
+++ Python001.py
@@ -3,3 +3,14 @@
 # Write a program in Python to check given number is prime or not.
 # Write a program in Python to find greatest among three integers.
 
+num = int(input("enter the number: "))
+if num>1:
+    for i in range(2,num/2):
+        if(num % i==0):
+           print(num,"the not number is Prime")
+           break
+        else :
+           print(num,"the  number is Prime")  
+    else :
+        print(num, "the number is not prime")   
+
